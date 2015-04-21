6103a1077b581f1109f30159e7aaa27200d5e922
gunapal <gunapal@gmail.com>
2015-04-21 00:04:25 -0700

    Hello world example using alert

---
+++ SimpleExamples/HelloWorld.js
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>Hello, Universe</title>
+	</head>
+	<body>
+		<script type="text/javascript">
+			alert("Hello, World")
+		</script>
+		<p id="insertText">
+
+		</p>
+	</body>
+</html>
